136fd173b242080a05207981c17ad3047836490f
rabeeh-ta <rabeehchemnad@gmail.com>
2022-10-30 14:32:22 +0530

    create txt func added

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import PyPDF2 
-    
-# creating a pdf file object 
-pdfFileObj = open('sample.pdf', 'rb') 
-    
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-    
-# printing number of pages in pdf file 
-print(pdfReader.numPages) 
-    
-# creating a page object 
-pageObj = pdfReader.getPage(0) 
-    
-# extracting text from page 
-print(pageObj.extractText()) 
-    
-# closing the pdf file object 
-pdfFileObj.close() 
+
+def saveTxtFile(txt):
+    f = open('sample.txt','w+')
+    f.write(txt)
+    f.close()
+
+def pdfToText(file):
+    # creating a pdf file object 
+    pdfFileObj = open(file, 'rb') 
+    # creating a pdf reader object 
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+    # creating a page object 
+    pageObj = pdfReader.getPage(0) 
+    # extracting text from page and pass to create txt func
+    saveTxtFile(pageObj.extractText()) 
+    # closing the pdf file object 
+    pdfFileObj.close() 
+
+
+
+pdfToText('./sample.pdf')
